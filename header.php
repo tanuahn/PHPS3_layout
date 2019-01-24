f3f69fa5ed607d0a3ee7554d9fbe0e961935959d
tanuahn <thieulatuananh92@gmail.com>
2019-01-25 00:54:11 +0700

    Layout

---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>City Gallery</h1>
+<style>
+    div.container {
+        width: 100%;
+        border: 1px solid gray;
+    }
+    header, footer {
+        padding: 1em;
+        color: white;
+        background-color: black;
+        clear: left;
+        text-align: center;
+    }
+</style>
+</body>
+</html>
+
